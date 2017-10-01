fbbd3940cd10cea973cc2fe490541c122b1c80ed
Rob Beal <rob@kohi.uk>
2017-10-01 12:48:30 +0100

    Fix the entrypoint args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'homeassistant' -a "$(id -u)" = '0' ]; then
+if [ "$1" = '/app/bin/hass' -a "$(id -u)" = '0' ]; then
     chown -R homeassistant /app
     chown -R homeassistant /config
     exec su-exec homeassistant "$@" $ARGS
