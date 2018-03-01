cfadbea03ee248d0202a1dd535602ae34e01b429
Robert Beal <robertbeal@users.noreply.github.com>
2018-03-01 19:31:37 +0000

    Fix the entrypoint's missing user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+!/bin/sh
 set -e
 
 if [ "$(id -u)" = '0' ]; then
     chown -R homeassistant /app
     chown -R homeassistant /config
-    exec su-exec python3 -m homeassistant "$@"
+    exec su-exec homeassistant /usr/bin/python3 -m homeassistant "$@"
 fi
 
-exec python3 -m homeassistant "$@"
+exec /usr/bin/python3 -m homeassistant "$@"
