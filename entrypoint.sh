e5443a21dcd0a1fae324a5a74d4192288d6849e5
Robert Beal <robertbeal@users.noreply.github.com>
2018-03-08 15:57:56 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 set -e
 
 if [ "$(id -u)" = '0' ]; then
-    chown -R homeassistant /app
-    chown -R homeassistant /config
     exec su-exec homeassistant /usr/bin/python3 -m homeassistant "$@"
 fi
 
